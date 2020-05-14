ad66328207c404d5748e622c463893ed1e26834b
Chua Zheng Leong <czl@iiyume.org>
2020-05-14 04:26:07 +0000

    Change user-worker comm(ec256), keygen -> enclave

--- app/src/aas_teaclave_ecall.rs
+++ app/src/aas_teaclave_ecall.rs
@@ -3,7 +3,8 @@ use advanca_crypto_ctypes::*;
 extern "C" {
     pub fn enclave_init_ra (eid: sgx_enclave_id_t, retval: *mut sgx_status_t, b_pse: i32, p_context: *mut sgx_ra_context_t) -> sgx_status_t;
     pub fn enclave_ra_close (eid: sgx_enclave_id_t, retval: *mut sgx_status_t, context: sgx_ra_context_t) -> sgx_status_t;
-    pub fn gen_worker_ec256_pubkey (eid: sgx_enclave_id_t, retval: *mut sgx_status_t) -> sgx_status_t;
+    pub fn gen_worker_ec256_pubkey (eid: sgx_enclave_id_t, retval: *mut sgx_status_t, worker_pubkey: &mut sgx_ec256_public_t) -> sgx_status_t;
     pub fn gen_worker_reg_request (eid: sgx_enclave_id_t, retval: *mut sgx_status_t, context: sgx_ra_context_t, aas_request: *mut CAasRegRequest) -> sgx_status_t;
-    pub fn compute_ec256_shared_dhkey (eid: sgx_enclave_id_t, retval: *mut sgx_status_t, context: sgx_ra_context_t, ephemeral_key: *const CSgxEphemeralKey) -> sgx_status_t;
+    pub fn accept_task (eid: sgx_enclave_id_t, retval: *mut sgx_status_t, task_id: &[u8;32], user_pubkey: &sgx_ec256_public_t) -> sgx_status_t;
+    pub fn encrypt_msg (eid: sgx_enclave_id_t, retval: *mut sgx_status_t, task_id: &[u8;32], msg_in: *const u8, msg_out: *mut u8, msg_len: u32) -> sgx_status_t;
 }
