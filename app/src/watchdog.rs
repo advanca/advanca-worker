62538a08912b070a38371432123d9ac37f86458b
Chua Zheng Leong <czl@iiyume.org>
2020-06-20 10:35:30 +0000

    Add watchdog implementation, prints timestamp

--- app/src/watchdog.rs
+++ app/src/watchdog.rs
@@ -4,7 +4,8 @@ use sp_runtime::generic;
 use std::sync::{Arc, Mutex};
 
 use sgx_types::*;
-
+use advanca_crypto_types::*;
+use aas_protos_std::aas::aas::*;
 use crate::enclave::heartbeat_challenge;
 use worker_protos_std::storage::storage::*;
 
@@ -13,7 +14,7 @@ use worker_protos_std::storage::storage::*;
 // To be replaced when types are separated from advanca-node.
 pub type Header = generic::Header<u32, BlakeTwo256>;
 
-pub fn watchdog_loop(task_id: [u8;32], eid: sgx_enclave_id_t, uri: &str, is_done: Arc<Mutex<bool>>) {
+pub fn watchdog_loop(task_id: [u8;32], eid: sgx_enclave_id_t, uri: &str, is_done: Arc<Mutex<bool>>, aas_client: Arc<Mutex<AasServerClient>>) {
     async_std::task::block_on(async move {
         let transport =
             jsonrpsee::transport::ws::WsTransportClient::new(uri)
@@ -37,7 +38,12 @@ pub fn watchdog_loop(task_id: [u8;32], eid: sgx_enclave_id_t, uri: &str, is_done
             heartbeat_req.task_id = task_id.to_vec();
             heartbeat_req.block_hash = block_hash.as_bytes().to_vec();
             let heartbeat_response = heartbeat_challenge(eid, heartbeat_req);
-            println!("heartbeat response: {:?}", heartbeat_response);
+            let mut timestamp_request = TimestampRequest::new();
+            timestamp_request.data = heartbeat_response.heartbeat_sig.to_owned();
+            // connect to aas to get a timestamp for heartbeat_response
+            let timestamp_response = aas_client.lock().unwrap().timestamp(&timestamp_request).unwrap();
+            let timestamp : Secp256r1SignedMsg = serde_cbor::from_slice(&timestamp_response.signed_data).unwrap();
+            println!("timestamp response: {:?}", timestamp);
             if *is_done.lock().unwrap() {
                 break;
             }
