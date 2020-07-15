b0cc978249ed868ec20cabed5633912eb7f6dcfd
Chua Zheng Leong <czl@iiyume.org>
2020-07-15 05:39:37 +0000

    Cargo fmt

--- app/build.rs
+++ app/build.rs
@@ -44,7 +44,6 @@ fn main() {
     println!("cargo:rustc-link-lib=static=sgx_uprotected_fs");
     println!("cargo:rustc-link-lib=static=sgx_ukey_exchange");
 
-
     match sgx_mode.as_ref() {
         "SW" => {
             println!("cargo:rustc-link-lib=dylib=sgx_urts_sim");
